e3998a0449cadbfefa57e9fcf9ee8f4d85206806
maylandertamas <maylandertamas@gmail.com>
2017-06-18 19:48:33 +0200

    Fixed decimals for diameter

--- get_api_data.py
+++ get_api_data.py
@@ -1,25 +1,37 @@
 import requests
+from collections import Counter
+
+
+def whole_float_to_int(float_number):
+    if float_number.is_integer():
+        whole_number = str(int(float_number))
+        return whole_number
+    else:
+        return float_number
 
 
 def planets_from_api(page_num):
-    planets_data_needed_from_api = ['name', 'diameter', 'climate', 'terrain', 'surface_water', 'population' 'url']
     all_planet_data = []
     response = requests.get('http://swapi.co/api/planets/?page=' + str(page_num)).json()
     response = response['results']
     for dictionary in response:
         actual_planet_data = {}
         for key, value in dictionary.items():
+
             if key == 'url':
                 if len(value) == 30:
-                    planet_id = value[-2]
+                    planet_id_from_url = value[-2]
                 else:
-                    planet_id = value[-3:-1]
-                actual_planet_data.update({'id': value})
+                    planet_id_from_url = value[-3:-1]
+                actual_planet_data.update({'id': planet_id_from_url})
+
             elif key == 'name' or key == 'climate' or key == 'terrain':
                 actual_planet_data.update({key: value})
+
             elif key == 'diameter':
                 if value.isnumeric():
-                    diameter_in_km = str(int(value) / 1000)
+                    diameter_in_km = int(value) / 1000
+                    diameter_in_km = whole_float_to_int(diameter_in_km)  # if it gets a whole float number convert it to simple integer
                     actual_planet_data.update({key: diameter_in_km})
                 else:
                     actual_planet_data.update({key: value})
@@ -35,16 +47,18 @@ def planets_from_api(page_num):
                     else:
                         surface_water_percentage = value + '%'
                         actual_planet_data.update({key: surface_water_percentage})
+
             elif key == 'population':
                 if value.isnumeric():
                     divide_population = float(value) / 1000000
-
-                    # if it gets a whole number convert it to simple integer
-                    if divide_population.is_integer():
-                        divide_population = str(int(divide_population))
+                    divide_population = whole_float_to_int(divide_population)  # if it gets a whole float number convert it to simple integer
                     actual_planet_data.update({key: divide_population})
                 else:
                     actual_planet_data.update({key: value})
 
+            elif key == 'residents':
+                number_of_residents = str(len(value))
+                actual_planet_data.update({key: number_of_residents})
         all_planet_data.append(actual_planet_data)
+    
     return all_planet_data
