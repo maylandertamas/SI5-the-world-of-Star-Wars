65fbcae2d4b30c5a7f67d6bfc977f9b5192cce97
maylandertamas <maylandertamas@gmail.com>
2017-06-18 15:33:00 +0200

    Aded table structure to print out API data

--- get_api_data.py
+++ get_api_data.py
@@ -1,5 +1,4 @@
 import requests
-from collections import Counter
 
 
 def get_planets_from_api(page_num):
@@ -8,28 +7,28 @@ def get_planets_from_api(page_num):
     response = requests.get('http://swapi.co/api/planets/?page=' + str(page_num)).json()
     response = response['results']
     for dictionary in response:
-        actual_planet_data = []
+        actual_planet_data = {}
         for key, value in dictionary.items():
             if key == 'url':
                 if len(value) == 30:
                     planet_id = value[-2]
                 else:
                     planet_id = value[-3:-1]
-                actual_planet_data.insert(0, planet_id)
-            elif key == 'name' or key == 'climate' or key == 'terrain':
-                actual_planet_data.append(value)
-            elif key == 'diameter':
+                actual_planet_data.update({'id': value})
+            if key == 'name' or key == 'climate' or key == 'terrain' or key == 'population':
+                actual_planet_data.update({key: value})
+            if key == 'diameter':
                 if value.isdigit():
                     diameter_in_km = str(int(value) / 1000)
-                    actual_planet_data.append(diameter_in_km)
+                    actual_planet_data.update({key: diameter_in_km})
                 else:
-                    actual_planet_data.append(value)
-            elif key == 'surface_water':
-                if value.isdigit():
+                    actual_planet_data.update({key: value})
+            if key == 'surface_water':
+                if not value.isdigit():
                     surface_water_percentage = value + '%'
-                    actual_planet_data.append(surface_water_percentage)
+                    actual_planet_data.update({key: surface_water_percentage})
                 else:
-                    actual_planet_data.append(value)
+                    actual_planet_data.update({key: value})
 
         all_planet_data.append(actual_planet_data)
     return all_planet_data
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<int:page>")
 def index(page=1):
-    planets_data = get_api_data.get_planets_from_api(page)
+    # planets_data = get_api_data.get_planets_from_api(page)
+    planets_data = [{'name': 'asdas', 'population': '124124', 'terrain': 'snowwie', 'diameter': '13.5', 'climate': 'hot', 'surface_water': '13%'},
+                    {'name': 'quack', 'population': '124124', 'terrain': 'snowwie', 'diameter': '13.5', 'climate': 'hot', 'surface_water': '13%'}]
     return render_template("page.html", planets_data=planets_data, page=page)
 
 
