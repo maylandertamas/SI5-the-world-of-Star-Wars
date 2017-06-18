6eeda2617f17e616e2644250d57810ee07857220
maylandertamas <maylandertamas@gmail.com>
2017-06-18 19:03:48 +0200

    Printing API data to website table is done

--- get_api_data.py
+++ get_api_data.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_planets_from_api(page_num):
+def planets_from_api(page_num):
     planets_data_needed_from_api = ['name', 'diameter', 'climate', 'terrain', 'surface_water', 'population' 'url']
     all_planet_data = []
     response = requests.get('http://swapi.co/api/planets/?page=' + str(page_num)).json()
@@ -15,18 +15,34 @@ def get_planets_from_api(page_num):
                 else:
                     planet_id = value[-3:-1]
                 actual_planet_data.update({'id': value})
-            if key == 'name' or key == 'climate' or key == 'terrain' or key == 'population':
+            elif key == 'name' or key == 'climate' or key == 'terrain':
                 actual_planet_data.update({key: value})
-            if key == 'diameter':
-                if value.isdigit():
+            elif key == 'diameter':
+                if value.isnumeric():
                     diameter_in_km = str(int(value) / 1000)
                     actual_planet_data.update({key: diameter_in_km})
                 else:
                     actual_planet_data.update({key: value})
-            if key == 'surface_water':
-                if not value.isdigit():
+            elif key == 'surface_water':
+                if value.isnumeric():
                     surface_water_percentage = value + '%'
                     actual_planet_data.update({key: surface_water_percentage})
+                else:
+                    try:
+                        float(value)
+                    except ValueError:
+                        actual_planet_data.update({key: value})
+                    else:
+                        surface_water_percentage = value + '%'
+                        actual_planet_data.update({key: surface_water_percentage})
+            elif key == 'population':
+                if value.isnumeric():
+                    divide_population = float(value) / 1000000
+
+                    # if it gets a whole number convert it to simple integer
+                    if divide_population.is_integer():
+                        divide_population = str(int(divide_population))
+                    actual_planet_data.update({key: divide_population})
                 else:
                     actual_planet_data.update({key: value})
 
--- main.py
+++ main.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<int:page>")
 def index(page=1):
-    # planets_data = get_api_data.get_planets_from_api(page)
-    planets_data = [{'name': 'asdas', 'population': '124124', 'terrain': 'snowwie', 'diameter': '13.5', 'climate': 'hot', 'surface_water': '13%'},
-                    {'name': 'quack', 'population': '124124', 'terrain': 'snowwie', 'diameter': '13.5', 'climate': 'hot', 'surface_water': '13%'}]
-    return render_template("page.html", planets_data=planets_data, page=page)
-
+    planets_data = []
+    try:
+        planets_data = get_api_data.planets_from_api(page)
+    except KeyError:
+        panets_data = None
+    planets_data_length = len(planets_data)
+    return render_template("page.html", planets_data=planets_data, page=page, planets_data_length=planets_data_length)
 
 if __name__ == '__main__':
     app.run(debug=True)
