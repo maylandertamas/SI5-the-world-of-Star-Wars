112e9c6cfc320e1764f02be7213946acf49cf601
maylandertamas <maylandertamas@gmail.com>
2017-06-19 12:04:07 +0200

    Started modal table print functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,41 @@
-/* function getResidentsDataFromApi () {
-$.getJSON('https://api.github.com/repos/atom/atom', function(response){
-    console.log(response['stargazers_count'])
-});
-}
-*/
-function main() {
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  // modal.find('.modal-title').text('New message to ' + recipient)
-  // modal.find('.modal-body input').val(recipient)
-});
+
+function printToModals(planetResidents) {
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var newResidentObj = new Object();
+    for (let i = 0; i < planetResidents.length; i++) {
+      $.getJSON(planetResidents[i], function(response){
+        newResidentObj['name'] = response['name'];
+        newResidentObj['height'] = response['height'];
+        newResidentObj['mass'] = response['mass'];
+        newResidentObj['skin_color'] = response['skin_color'];
+        newResidentObj['hair_color'] = response['hair_color'];
+        newResidentObj['eye_color'] = response['eye_color'];
+        newResidentObj['birth_year'] = response['birth_year'];
+        newResidentObj['gender'] = response['gender'];
+        
+      });
+    }
+    console.log(newResidentObj);
     
+    $(this).find('.modal-body tbody')
+          .append('<tr>')
+          .append('<td>' + newResidentObj[name] + '<td>');
+});
 }
+
+function main() {
+  /*
+$("#exampleModal").on("hidden.bs.modal", function(){
+    $(".modal-body").html("");
+}); */
+
+$('.residents-button').click(function() {
+  var planetResidents = $(this).data('value');
+  printToModals(planetResidents);
+});
+
+
+}
+
 
 $(document).ready(main);
