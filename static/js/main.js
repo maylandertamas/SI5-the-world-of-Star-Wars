e55c8664da75fba6c415a0df6251d5388ffa1014
maylandertamas <maylandertamas@gmail.com>
2017-06-19 18:02:53 +0200

    Started to solve listing bug.

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,56 @@
 
 function printToModals(planetResidents) {
-  $('#exampleModal').on('show.bs.modal', function (event) {
-    var newResidentObj = new Object();
-    for (let i = 0; i < planetResidents.length; i++) {
-      $.getJSON(planetResidents[i], function(response){
-        newResidentObj['name'] = response['name'];
-        newResidentObj['height'] = response['height'];
-        newResidentObj['mass'] = response['mass'];
-        newResidentObj['skin_color'] = response['skin_color'];
-        newResidentObj['hair_color'] = response['hair_color'];
-        newResidentObj['eye_color'] = response['eye_color'];
-        newResidentObj['birth_year'] = response['birth_year'];
-        newResidentObj['gender'] = response['gender'];
-        
-      });
-    }
-    console.log(newResidentObj);
-    
-    $(this).find('.modal-body tbody')
-          .append('<tr>')
-          .append('<td>' + newResidentObj[name] + '<td>');
-});
+  
 }
 
 function main() {
-  /*
-$("#exampleModal").on("hidden.bs.modal", function(){
-    $(".modal-body").html("");
-}); */
 
-$('.residents-button').click(function() {
-  var planetResidents = $(this).data('value');
-  printToModals(planetResidents);
-});
+  $("#exampleModal").on("hidden.bs.modal", function(){
+      $(".modal-body tbody").empty("");
+  });
+
+  $('.residents-button').click(function() {
+    var planetResidents = [];
+    var buttonId = $(this).id;
+    var planetResidents = $(this).data('value');
+    $('#exampleModal').on('show.bs.modal', function (event) {
+    for (let i = 0; i < planetResidents.length; i++) {
+      console.log(planetResidents[i]);
+      var jsonFile = planetResidents[i];
+      $.ajax({
+          async: true,
+          dataType: "json",
+          url: jsonFile,
+          cache: false,
+          success: function(response) {
+              response['name']
+              response['height']
+              response['mass'] 
+              response['skin_color]
+              response['hair_col
+              response['eye_colo
+              response['birth_ye
+              response['gender']
+          $('#exampleModal').find('.modal-body tbody')
+          .append('<tr>')
+          .append('<td>' + response['name'] + '</td>')
+          .append('<td>' + response['height'] + '</td>')
+          .append('<td>' + response['mass']+ '</td>')
+          .append('<td>' + response['skin_color'] + '</td>')
+          .append('<td>' + response['hair_color'] + '</td>')
+          .append('<td>' + response['eye_color'] + '</td>')
+          .append('<td>' + response['birth_year'] + '</td>')
+          .append('<td>' + response['gender'] + '</td>')
+          .append('</tr>');
+          planetResidents.splice (jsonFile, 1);
+          localStorage.setItem('shit', jsonFile);
+          localStorage.getItem('shit');
+        }
+      });
+    
+    }
+  });
+  });
 
 
 }
