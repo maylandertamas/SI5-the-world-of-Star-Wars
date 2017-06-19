c88438b7b5bf69a8b2118a91a8d4554887698f1e
maylandertamas <maylandertamas@gmail.com>
2017-06-19 22:29:39 +0200

    Structured database handler functions

--- database_handler.py
+++ database_handler.py
@@ -4,7 +4,7 @@ import sys
 
 def connect_to_database():
     try:
-        conn = psycopg2.connect("dbname='' user=''")
+        conn = psycopg2.connect("dbname='SI5_starwars' user=''")
     except psycopg2.Error as error:
         print("Couldn't connect to database")
         print(error)
@@ -12,3 +12,19 @@ def connect_to_database():
     else:
         return conn
 
+
+def database_handler(command, command_type="read"):
+    conn = connect_to_database()
+    cur = conn.cursor()
+    with conn:
+        with cur:
+            if command_type == "read":
+                cur.execute(command)
+                fetch_data = cur.fetchall()
+                return fetch_data
+            elif command_type == "write":
+                cur.execute(command)
+            else:
+                pass
+
+    
