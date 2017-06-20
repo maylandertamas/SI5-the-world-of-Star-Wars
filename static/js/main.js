f2f7c45cede297c28d010d5e762d9a885c1e6b71
maylandertamas <maylandertamas@gmail.com>
2017-06-20 18:36:46 +0200

    Implemented modal body connected with the appropriate flask (form) route

--- static/js/main.js
+++ static/js/main.js
@@ -79,22 +79,58 @@ function printToModals() {
 }
 
 
-/*
+
 function loginModal() {
+
    $('#loginModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
+    var buttonData = button.attr('id');
+    if (buttonData == 'registration') {
+      $('#loginModal').find('.modal-body')
+        .append('<form action="/registration" method="post">\
+                <div class="form-group">\
+                    <label for="recipient-name" class="control-label">Username:</label>\
+                    <input type="text" class="form-control" id="user-name" name="username" required>\
+                </div>\
+                <div class="form-group">\
+                    <label for="message-text" class="control-label">Password:</label>\
+                    <textarea class="form-control" id="password-text" name="password" required></textarea>\
+                </div>\
+                <div class="modal-footer">\
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                    <button type="submit" class="btn btn-primary">Registration</button>\
+                </div>\
+                </form>')
+      $('#loginModal').find('.modal-header').append("<h2>Registration</h2>")
 
-    });
-} */
-/*
-function regModal() {
-   $('#loginModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-
-    });
-} */
+    } else if (buttonData === 'login') {
+      $('#loginModal').find('.modal-body')
+    .append('<form action="/login" method="post">\
+                <div class="form-group">\
+                    <label for="recipient-name" class="control-label" name="">Username:</label>\
+                    <input type="text" class="form-control" id="user-name" name="username" required>\
+                </div>\
+                <div class="form-group">\
+                    <label for="message-text" class="control-label">Password:</label>\
+                    <textarea class="form-control" id="password-text" name="password"required></textarea>\
+                </div>\
+                <div class="modal-footer">\
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                    <button type="submit" class="btn btn-primary">Login</button>\
+                </div>\
+                </form>')
+       $('#loginModal').find('.modal-header').append("<h2>Login</h2>")
+    }
+    
+  });
+}
 
 function main() {
+  $("#loginModal").on("hidden.bs.modal", function(){
+    $(".modal-body").empty("");
+    $(".modal-header").empty("");
+  });
+
 
   $("#exampleModal").on("hidden.bs.modal", function(){
       $(".modal-body tbody").empty("");
@@ -102,6 +138,7 @@ function main() {
 
   printToModals();
 
+  loginModal();
 
 
 }
