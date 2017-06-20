37835a03174237442a968bccc280ed876f6302e7
maylandertamas <maylandertamas@gmail.com>
2017-06-20 14:28:38 +0200

    Started modal window modifications

--- static/js/main.js
+++ static/js/main.js
@@ -79,17 +79,20 @@ function printToModals() {
 }
 
 
-
+/*
 function loginModal() {
-   $('#login').click(function() {
-    $('#loginModal').on('show.bs.modal', function (event) {
-       // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this)
+   $('#loginModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+
     });
-  });
-}
+} */
+/*
+function regModal() {
+   $('#loginModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+
+    });
+} */
 
 function main() {
 
