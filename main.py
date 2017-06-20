03cb710c965ee7f9cb00f279ef5d4a313ba259b8
maylandertamas <maylandertamas@gmail.com>
2017-06-20 14:28:03 +0200

    Login functional

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, escape, request
 import get_api_data
 from database_handler import database_handler
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/<int:page>")
 def index(username=None, page=1):
-    print(session)
-    """if username not in session:
+    if 'username' in session:
+        username = session['username']
+    else:
         username = None
-    """
     try:
         planets_data = get_api_data.planets_from_api(page)
     except KeyError:
@@ -21,25 +20,23 @@ def index(username=None, page=1):
     return render_template("page.html", planets_data=planets_data, page=page, planets_data_length=planets_data_length, username=username)
 
 
-@app.route('/checkuser')
-def checkuser():
+@app.route('/registration', methods=['POST'])
+def registration():
    pass
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        username = request.form['username']
-        return redirect(url_for('index'))
+    session['username'] = request.form['username']
+    password = request.form['password']
+    hashed_pass = generate_password_hash(password)
+    return redirect(url_for('index'))
 
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
     session.pop('username', None)
-    username = None
-    return redirect(url_for('index', username=username))
+    return redirect(url_for('index'))
 
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
