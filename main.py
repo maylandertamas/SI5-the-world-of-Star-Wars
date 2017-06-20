298b7fc4ab48b50c1ba33cf3e0d222da20b5e0a3
maylandertamas <maylandertamas@gmail.com>
2017-06-20 18:31:56 +0200

    Registration and login  methods implemented

--- main.py
+++ main.py
@@ -22,14 +22,36 @@ def index(username=None, page=1):
 
 @app.route('/registration', methods=['POST'])
 def registration():
-   pass
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password,"pbkdf2:sha224", 1)
+
+    write_to_database = database_handler("INSERT INTO user_table (username, password)\
+                    VALUES ($${0}$$, $${1}$$);".format(username, hashed_password), "write")
+    if write_to_database == "error":
+        return '<a href="/"><button href= class="btn btn-default">Back</button></a><p>Username already exists or too long<p>'
+    return redirect(url_for('index'))
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    session['username'] = request.form['username']
+    username = request.form['username']
     password = request.form['password']
-    hashed_pass = generate_password_hash(password)
+    user_db_data = database_handler("SELECT username, password FROM user_table\
+                        WHERE username='{0}';".format(username))
+    try:
+        username_from_db = user_db_data[0][0]
+    except IndexError:
+        return '<a href="/"><button href= class="btn btn-default">Back</button></a><p>Username not found<p>' 
+    else:
+        user_pw_from_db = user_db_data[0][1]
+        remove_white_spaces_from_pw = user_pw_from_db.strip()
+        password_check = check_password_hash(remove_white_spaces_from_pw, password)
+        if password_check:
+            session['username'] = username_from_db
+        else:
+            return '<a href="/"><button href= class="btn btn-default">Back</button></a><p>Invalid password<p>'
+
     return redirect(url_for('index'))
 
 
